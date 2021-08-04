ea2eef13dc9acf68a1765b71ec179b23d6d53751
BigBigMouse <2292199665@qq.com>
2021-08-04 13:33:49 +0800

    修改登录拦截器

--- iDrink/src/main/java/com/iDrink/interceptor/LoginInterceptor.java
+++ iDrink/src/main/java/com/iDrink/interceptor/LoginInterceptor.java
@@ -2,15 +2,12 @@ package com.iDrink.interceptor;
 //自定义拦截器
 
 import com.iDrink.pojo.Administrator;
-import com.iDrink.pojo.Customer;
-import com.iDrink.pojo.Product;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.function.Supplier;
 
 /**
  * 登录拦截器
@@ -27,11 +24,8 @@ public class LoginInterceptor implements HandlerInterceptor {
 		// 获取Session
 		HttpSession session = request.getSession();
 		Administrator administrator = (Administrator) session.getAttribute("USER_SESSION");
-		Supplier supplier = (Supplier) session.getAttribute("SUP_SESSION");
-		Customer customer = (Customer) session.getAttribute("CUS_SESSION");
-		Product product = (Product) session.getAttribute("PRO_SESSION");
 		// 判断Session中是否有用户数据，如果有，则返回true,继续向下执行
-		if (administrator != null || supplier != null || customer != null || product != null) {
+		if (administrator != null) {
 			return true;
 		}
 		// 不符合条件的给出提示信息，并转发到登录页面
